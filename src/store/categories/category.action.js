4beb3e58e7ad9b77778f793e9aa6096b252b6730
Saul Pereira <saul_pereira@hotmail.com>
2023-09-08 16:40:28 -0600

    172. Redux-Thunk Pt.2

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,4 +1,23 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-export const setCategories = (categoriesArray) => 
-    ({ type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES, payload: categoriesArray});
+import {  getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export const fetchCategoriesStart = () => 
+    ({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START });
+
+export const fetchCategoriesSuccess = (categoriesArray) => 
+    ({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, payload: categoriesArray });
+
+export const fetchCategoriesFailed = (error) => 
+    ({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, payload: error });
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+        const categoriesArray  = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch(error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+    
+}
