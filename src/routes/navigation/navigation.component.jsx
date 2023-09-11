4d7294806c908dbc2fcb928161773ce25399bed4
Saul Pereira <saul_pereira@hotmail.com>
2023-09-11 16:05:52 -0600

    182. Redux-Saga
    182. Redux-Saga: Sign Out Sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,26 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { selectIsCartOpen } from '../../store/cart/cart.selector.js';
+import { signOutStart } from '../../store/user/user.action';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink }  from './navigation.styles';
 
 const Navigation = () => {
+    const dispatch = useDispatch();
     //const { currentUser } = useContext(UserContext);
     const currentUser = useSelector(selectCurrentUser);
     //const { isCartOpen } = useContext(CartContext);
     const isCartOpen = useSelector(selectIsCartOpen);
+
+    const signOutUser = () => dispatch(signOutStart());
 
     return (
         <Fragment>
--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -18,3 +18,9 @@ export const signUpStart = (email, password, displayName ) => ({ type: USER_ACTI
 export const signUpSuccess = (user, additionalDetails) => ({ type: USER_ACTION_TYPES.SIGN_UP_SUCCESS, payload: { user, additionalDetails }});
 
 export const signUpFailed = (error) => ({ type: USER_ACTION_TYPES.SIGN_UP_FAILED, payload: error });
+
+export const signOutStart = () => ({ type: USER_ACTION_TYPES.SIGN_OUT_START });
+
+export const signOutSuccess = () => ({ type: USER_ACTION_TYPES.SIGN_OUT_SUCCESS });
+
+export const signOutFailed = (error) => ({ type: USER_ACTION_TYPES.SIGN_OUT_FAILED, payload: error });
