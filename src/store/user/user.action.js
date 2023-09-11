d114d1ab348a251dd918f4a139bb6864df3a0fe7
Saul Pereira <saul_pereira@hotmail.com>
2023-09-11 12:03:27 -0600

    178. Redux-Saga
    178. Redux-Saga: Check User Session Saga Pt.1

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -2,3 +2,13 @@ import { USER_ACTION_TYPES } from "./user.types";
 
 export const setCurrentUser = (user) => 
     ({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+
+export const checkUserSession = () => ({ type: USER_ACTION_TYPES.CHECK_USER_SESSION});
+
+export const googleSignInStart = () => ({ type: USER_ACTION_TYPES.GOOGLE_SIGN_IN_START });
+
+export const emailSignInStart = (email, password) => ({ type: USER_ACTION_TYPES.EMAIL_SIGN_IN_START, payload: {email, password}});
+
+export const signInSuccess = (user) => ({ type: USER_ACTION_TYPES.SIGN_IN_SUCCESS, payload: user });
+
+export const signInFailed = (error) => ({ type: USER_ACTION_TYPES.SIGN_IN_FAILED, payload: error });
