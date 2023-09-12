91da30d43ac959e02bdd3026a3d6352fb43d23e9
Saul Pereira <saul_pereira@hotmail.com>
2023-09-12 12:54:30 -0600

    195. Finishing Stripe Payment
    195. Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,12 +15,39 @@ const PaymentForm = () => {
             return;
         }
 
-        
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000})
+        }).then(res => res.json());
+
+        const {paymentIntent: { client_secret }} = response;
+
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Saul Pereira'
+                }
+            }
+        })
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successfull');
+            }
+        }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
